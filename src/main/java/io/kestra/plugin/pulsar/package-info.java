470c950d6eb607338d9695e9fe3a794edfc70b47
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:36:28 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/pulsar/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using the Apache Pulsar message broker.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.pulsar;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
