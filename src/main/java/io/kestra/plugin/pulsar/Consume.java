2760aa1494b007c8172a4e556c5ecd80987443bb
iNikitaGricenko <VfVristalt@gmail.com>
2024-05-08 15:34:10 +0300

    Implement RealtimeTrigger

--- src/main/java/io/kestra/plugin/pulsar/Consume.java
+++ src/main/java/io/kestra/plugin/pulsar/Consume.java
@@ -5,13 +5,27 @@ import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.utils.Rethrow;
+import io.micronaut.core.exceptions.ExceptionHandler;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.shade.org.apache.avro.generic.GenericDatumReader;
+import org.apache.pulsar.shade.org.apache.avro.generic.GenericDatumWriter;
+import org.apache.pulsar.shade.org.apache.avro.io.*;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.AbstractMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -39,6 +53,9 @@ import static io.kestra.core.utils.Rethrow.throwFunction;
 )
 public class Consume extends AbstractReader implements RunnableTask<AbstractReader.Output>, SubscriptionInterface {
     private String subscriptionName;
+
+    @Builder.Default
+    private SerdeType deserializer = SerdeType.STRING;
 
     @Builder.Default
     private SubscriptionInitialPosition initialPosition = SubscriptionInitialPosition.Earliest;
@@ -103,5 +120,103 @@ public class Consume extends AbstractReader implements RunnableTask<AbstractRead
                 );
             }
         }
+    }
+
+    public Publisher<PulsarMessage> stream(RunContext runContext) {
+        return Flux.<PulsarMessage>create(
+                sink -> {
+                    try (PulsarClient client = PulsarService.client(this, runContext)) {
+                        ConsumerBuilder<byte[]> consumerBuilder = client.newConsumer()
+                            .topics(this.topics(runContext))
+                            .subscriptionName(runContext.render(this.subscriptionName))
+                            .subscriptionInitialPosition(this.initialPosition)
+                            .subscriptionType(this.subscriptionType);
+
+                        if (this.consumerName != null) {
+                            consumerBuilder.consumerName(runContext.render(this.consumerName));
+                        }
+
+                        if (this.consumerProperties != null) {
+                            consumerBuilder.properties(this.consumerProperties
+                                .entrySet()
+                                .stream()
+                                .map(
+                                    throwFunction(e -> new AbstractMap.SimpleEntry<>(
+                                            runContext.render(e.getKey()),
+                                            runContext.render(e.getValue())
+                                        )
+                                    )
+                                )
+                                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                            );
+                        }
+
+                        if (this.encryptionKey != null) {
+                            consumerBuilder.defaultCryptoKeyReader(runContext.render(this.encryptionKey));
+                        }
+
+                        try (Consumer<byte[]> consumer = consumerBuilder.subscribe()) {
+                            StreamSupport
+                                .stream(consumer.batchReceive().spliterator(), false)
+                                .map(
+                                    throwFunction(message -> {
+                                            consumer.acknowledge(message);
+
+                                            return message;
+                                        }
+                                    )
+                                )
+                                .map(buildMessage(sink::error))
+                                .forEach(sink::next);
+                        }
+                    } catch (Throwable throwable) {
+                        sink.error(throwable);
+                    } finally {
+                        sink.complete();
+                    }
+                },
+                FluxSink.OverflowStrategy.BUFFER
+            )
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    private Function<Message<byte[]>, PulsarMessage> buildMessage(ExceptionHandler<Throwable> onException) {
+        return message -> {
+            boolean applySchema = this.schemaType != SchemaType.NONE;
+            if (applySchema && this.schemaString == null) {
+                onException.handle(
+                    new IllegalArgumentException("Must pass a \"schemaString\" when the \"schemaType\" is not null")
+                );
+            }
+
+            PulsarMessage.PulsarMessageBuilder messageBuilder =
+                PulsarMessage.builder()
+                    .key(message.getKey())
+                    .properties(message.getProperties())
+                    .topic(message.getTopicName());
+
+            try {
+                messageBuilder.value(applySchema ? this.deserializeWithSchema(message.getValue()) : this.deserializer.deserialize(message.getValue()));
+            } catch (Throwable e) {
+                onException.handle(e);
+            }
+
+            if (message.getEventTime() != 0) {
+                messageBuilder.eventTime(Instant.ofEpochMilli(message.getEventTime()));
+            }
+
+            return messageBuilder.messageId(message.getMessageId().toString()).build();
+        };
+    }
+
+    @Getter
+    @Builder
+    public static class PulsarMessage implements io.kestra.core.models.tasks.Output {
+        private String key;
+        private Object value;
+        private Map<String, String> properties;
+        private String topic;
+        private Instant eventTime;
+        private String messageId;
     }
 }
---
+++ src/main/java/io/kestra/plugin/pulsar/RealtimeTrigger.java
@@ -0,0 +1,123 @@
+package io.kestra.plugin.pulsar;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.conditions.ConditionContext;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.triggers.*;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "React and consume messages from a Pulsar topic."
+)
+@Plugin(
+    examples = {
+        @Example(
+            code = {
+                "interval: PT30S",
+                "topic: kestra_trigger",
+                "uri: pulsar://localhost:26650",
+                "deserializer: JSON",
+                "subscriptionName: kestra_trigger_sub",
+            }
+        )
+    }
+)
+public class RealtimeTrigger extends AbstractTrigger implements RealtimeTriggerInterface, TriggerOutput<AbstractReader.Output>, PulsarConnectionInterface, SubscriptionInterface, ReadInterface {
+    @Builder.Default
+    private final Duration interval = Duration.ofSeconds(60);
+
+    private String uri;
+
+    private String authenticationToken;
+
+    private AbstractPulsarConnection.TlsOptions tlsOptions;
+
+    private Object topic;
+
+    @Builder.Default
+    private SerdeType deserializer = SerdeType.STRING;
+
+    @Builder.Default
+    private Duration pollDuration = Duration.ofSeconds(2);
+
+    private Integer maxRecords;
+
+    private Duration maxDuration;
+
+    private String subscriptionName;
+
+    @Builder.Default
+    private SubscriptionInitialPosition initialPosition = SubscriptionInitialPosition.Earliest;
+
+    @Builder.Default
+    private SubscriptionType subscriptionType = SubscriptionType.Exclusive;
+
+    private Map<String, String> consumerProperties;
+
+    private String encryptionKey;
+
+    private String consumerName;
+
+    @Schema(
+        title = "JSON string of the topic's schema",
+        description = "Required for connecting with topics with a defined schema and strict schema checking"
+    )
+    @PluginProperty(dynamic = true)
+    protected String schemaString;
+
+    @Schema(
+        title = "The schema type of the topic",
+        description = "Can be one of NONE, AVRO or JSON. None means there will be no schema enforced."
+    )
+    @PluginProperty(dynamic = true)
+    @Builder.Default
+    protected SchemaType schemaType = SchemaType.NONE;
+
+    @Override
+    public Publisher<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) throws Exception {
+        Consume task = Consume.builder()
+            .id(this.id)
+            .type(Consume.class.getName())
+            .uri(this.uri)
+            .authenticationToken(this.authenticationToken)
+            .tlsOptions(this.tlsOptions)
+            .topic(this.topic)
+            .deserializer(this.deserializer)
+            .pollDuration(this.pollDuration)
+            .maxRecords(this.maxRecords)
+            .maxDuration(this.maxDuration)
+            .subscriptionName(this.subscriptionName)
+            .initialPosition(this.initialPosition)
+            .subscriptionType(this.subscriptionType)
+            .consumerProperties(this.consumerProperties)
+            .encryptionKey(this.encryptionKey)
+            .consumerName(this.consumerName)
+            .schemaString(this.schemaString)
+            .schemaType(this.schemaType)
+            .build();
+
+        return Flux.from(task.stream(conditionContext.getRunContext()))
+            .map(message -> TriggerService.generateRealtimeExecution(this, context, message))
+            .next();
+    }
+}
+
