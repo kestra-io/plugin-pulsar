f9d22067abf9d637c2ae5cac7e1735f36efb4fb3
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-14 18:34:40 +0530

    test: use KestraTest scheduler for trigger tests

--- src/test/java/io/kestra/plugin/pulsar/TriggerTest.java
+++ src/test/java/io/kestra/plugin/pulsar/TriggerTest.java
@@ -2,7 +2,6 @@ package io.kestra.plugin.pulsar;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -16,14 +15,8 @@ import io.kestra.core.models.property.Property;
 import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.FlowListeners;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.TestsUtils;
-import io.kestra.jdbc.runner.JdbcScheduler;
-import io.kestra.scheduler.AbstractScheduler;
-import io.kestra.worker.DefaultWorker;
-
-import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import reactor.core.publisher.Flux;
@@ -32,14 +25,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
-@KestraTest
+@KestraTest(startRunner = true, startScheduler = true)
 class TriggerTest {
-    @Inject
-    private ApplicationContext applicationContext;
-
-    @Inject
-    private FlowListeners flowListenersService;
-
     @Inject
     @Named(QueueFactoryInterface.EXECUTION_NAMED)
     private QueueInterface<Execution> executionQueue;
@@ -51,63 +38,46 @@ class TriggerTest {
 
     @Test
     void flow() throws Exception {
-        // mock flow listeners
         CountDownLatch queueCount = new CountDownLatch(1);
+        Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
+            queueCount.countDown();
+            assertThat(execution.getLeft().getFlowId(), is("trigger"));
+        });
 
-        // scheduler
-        try (DefaultWorker worker = applicationContext.createBean(DefaultWorker.class, UUID.randomUUID().toString(), 8, null)) {
-            try (
-                AbstractScheduler scheduler = new JdbcScheduler(
-                    this.applicationContext,
-                    this.flowListenersService
-                );
-            ) {
-                // wait for execution
-                Flux<Execution> receive = TestsUtils.receive(executionQueue, execution ->
-                {
-                    queueCount.countDown();
-                    assertThat(execution.getLeft().getFlowId(), is("trigger"));
-                });
+        Produce task = Produce.builder()
+            .id(TriggerTest.class.getSimpleName())
+            .type(Produce.class.getName())
+            .uri(Property.ofValue("pulsar://localhost:26650"))
+            .serializer(Property.ofValue(SerdeType.JSON))
+            .topic(Property.ofValue("tu_trigger"))
+            .from(
+                List.of(
+                    ImmutableMap.builder()
+                        .put("key", "key1")
+                        .put("value", "value1")
+                        .build(),
+                    ImmutableMap.builder()
+                        .put("key", "key2")
+                        .put("value", "value2")
+                        .build()
+                )
+            )
+            .build();
 
-                Produce task = Produce.builder()
-                    .id(TriggerTest.class.getSimpleName())
-                    .type(Produce.class.getName())
-                    .uri(Property.ofValue("pulsar://localhost:26650"))
-                    .serializer(Property.ofValue(SerdeType.JSON))
-                    .topic(Property.ofValue("tu_trigger"))
-                    .from(
-                        List.of(
-                            ImmutableMap.builder()
-                                .put("key", "key1")
-                                .put("value", "value1")
-                                .build(),
-                            ImmutableMap.builder()
-                                .put("key", "key2")
-                                .put("value", "value2")
-                                .build()
-                        )
-                    )
-                    .build();
+        repositoryLoader.load(
+            Objects.requireNonNull(
+                TriggerTest.class.getClassLoader()
+                    .getResource("flows/trigger.yaml")
+            )
+        );
 
-                worker.run();
-                scheduler.run();
+        task.run(TestsUtils.mockRunContext(runContextFactory, task, ImmutableMap.of()));
 
-                repositoryLoader.load(
-                    Objects.requireNonNull(
-                        TriggerTest.class.getClassLoader()
-                            .getResource("flows/trigger.yaml")
-                    )
-                );
+        boolean await = queueCount.await(1, TimeUnit.MINUTES);
+        assertThat(await, is(true));
 
-                task.run(TestsUtils.mockRunContext(runContextFactory, task, ImmutableMap.of()));
+        Integer trigger = (Integer) Objects.requireNonNull(receive.blockLast()).getTrigger().getVariables().get("messagesCount");
 
-                boolean await = queueCount.await(1, TimeUnit.MINUTES);
-                assertThat(await, is(true));
-
-                Integer trigger = (Integer) Objects.requireNonNull(receive.blockLast()).getTrigger().getVariables().get("messagesCount");
-
-                assertThat(trigger, greaterThanOrEqualTo(2));
-            }
-        }
+        assertThat(trigger, greaterThanOrEqualTo(2));
     }
 }
